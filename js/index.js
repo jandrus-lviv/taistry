a01c2b819f2f337ebc607b7e3ef37d21583a5247
Andy <andriy.shevtsiv@gmail.com>
2019-03-14 13:22:49 +0000

    edited-19-03-14

--- js/index.js
+++ js/index.js
@@ -1,5 +1,2 @@
-const calendarPlaceholder = document.querySelector('.calendar');
-setInterval(() => calendarPlaceholder.innerText = (new Date()).toLocaleDateString(), 1000);
-
 const clockPlaceholder = document.querySelector('.clock');
 setInterval(() => clockPlaceholder.innerText = (new Date()).toLocaleTimeString(), 1000);
