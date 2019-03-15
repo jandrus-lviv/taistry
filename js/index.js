ec4d6b978381a148c1b60a6a077934cfc2cd632e
Andy <andriy.shevtsiv@gmail.com>
2019-03-15 10:51:37 +0000

    edited-19-03-15-12-51

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 const clockPlaceholder = document.querySelector('.clock');
 setInterval(() => clockPlaceholder.innerText = (new Date()).toLocaleTimeString(), 1000);
+
+$(function() {
+    $('.smooth').on('click', function(event) {
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
